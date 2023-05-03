6cbfea050c5022a3322f91f3d396c6b3d71d80bd
exam <exam@Bcalab4-m02>
2023-05-03 09:56:04 +0530

    firs commit

---
+++ casetoolsexam/src/casetoolsexam/harddisk_102.java
@@ -0,0 +1,15 @@
+package casetoolsexam;
+
+public class harddisk_102 {
+	int size;
+	harddisk(int size)
+	{
+		this.size=size;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
